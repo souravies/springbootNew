6f9f4df224e3962b5b832a24588560f6f542764f
sourav <saurabhkr573@gmail.com>
2021-05-20 21:12:32 +0530

    main class modified

--- src/main/java/com/example/saurav/it/SpringBootExample1Application.java
+++ src/main/java/com/example/saurav/it/SpringBootExample1Application.java
@@ -1,8 +1,9 @@
 package com.example.saurav.it;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootExample1Application {
 
